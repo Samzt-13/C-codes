51b6ac473146034fd655cf7ddfbcb2f6e860225c
Samzt-13 <code.samzt13@gmail.com>
2025-02-17 10:05:32 +0545

    ADD
    add hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char name[30];
+
+    printf("Please Enter your name to start: ");
+    scanf("%s", name);
+
+    printf("Hello %s, This is me Samrat. You can call me Sam.\n", name);
+    printf("In this file, I will include all what i have in my C-Codes with a bit of their intro.");
+
+    printf("\n\t\t\tC - CODES\n");
+    
+    return 0;
+}
