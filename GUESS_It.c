63ed71a550e971cf38da40a853436f01bd8d656c
Samzt-13 <code.samzt13@gmail.com>
2025-03-07 08:40:39 +0545

    Guess it
    Just added some comments for better understanding

--- GUESS_It.c
+++ GUESS_It.c
@@ -1,40 +1,50 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+//time.h & stdlib for random number generation
 
+//function prototypes
 void get_gamemode(int *);
 int check_and_gen(int);
-
+//starting main function
 int main(){
     int number, user_guess, attempts = 0, game_mode;
+    //declaring variables   
     srand(time (0));
+    //generating random number seed
     printf("\t\t\t\t\tGUESS IT\n");
     printf("Welcome to \"Guess it\" its a guessing game\n");
     printf("You will be given hints. Just try to guess the number: \n");
+    //Intro for the user
     get_gamemode(&game_mode);
+    //to get gamemode info
     number = check_and_gen(game_mode);
+    //generate random number according to user's gamemode selection
     do{
         printf("Guess a number: ");
         scanf("%d", &user_guess);
         attempts++;
         if(user_guess < number){
-            printf("Guess a higher number\n");
+            printf("Guess a higher number\n"); //asks for a higher number if needed
         }
         else if(user_guess > number){
-            printf("Guess a lower number\n");
+            printf("Guess a lower number\n"); //asks for a lower number if needed
         }
         else{
             printf("Congrats! You guessed it, it was %d\n", number);
             printf("Number of attempts taken: %d\n", attempts);
+            //congratulates the user & shows the number of attempts taken
         }
     }
     while(user_guess != number);
+    //do while loop to ask for user input & ask the user to make a guess
     
     printf("Thanks for playing my time pass game\n");
     printf("Developed by--Samrat Parajuli\n");
+    //just an ending note
     return 0;
 }
-
+//function to get gamemode
 void get_gamemode(int *a){
     int mode;
     printf("Enter game level first: \n");
@@ -50,7 +60,7 @@ void get_gamemode(int *a){
         get_gamemode(&mode);
     }
 }
-
+//function to check & generate random number
 int check_and_gen(int x){
     int number;
     switch(x){
