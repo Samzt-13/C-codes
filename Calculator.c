fc02f70ee263ebc7510fe323161a489f58cb62f9
Samzt-13 <code.samzt13@gmail.com>
2025-03-08 18:54:16 +0545

    Made the UI more better
    Made it user friendly and possible to add more feature in the future

--- Calculator.c
+++ Calculator.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
+int PrintMenu();
 
 void Sum(double, double);
 void Diff(double, double);
@@ -10,36 +13,34 @@ void factorial(double);
 
 int main(){
     double operand1, operand2;
-    char operator;
+    int power_call;
+    char operator = 1;
 
-    printf("\t\t\tSimple Calculator\n");    //title
-    //ask for an operator
-    printf("Enter an operator(+, -. *, /, ^, !): ");
-    scanf("%c", &operator);
+    operator = PrintMenu(); //get the operator & print the menu
     //switch statement for operator
     switch(operator){
-        case '+':{
+        case 1:{
             printf("Enter two numbers: ");
             scanf("%lf %lf", &operand1, &operand2);
             Sum(operand1, operand2);
             break;
         }
             //sum of two numbers
-        case '-':{
+        case 2:{
             printf("Enter two numbers: ");
             scanf("%lf %lf", &operand1, &operand2);
             Diff(operand1, operand2);
             break;
         }
             //difference of two numbers
-        case '*':{
+        case 3:{
             printf("Enter two numbers: ");
             scanf("%lf %lf", &operand1, &operand2);
             Mult(operand1, operand2);
             break;
         }
             //product of two numbers
-        case '/':{
+        case 4:{
             printf("Enter two numbers: ");
             scanf("%lf %lf", &operand1, &operand2);
             if(operand2 != 0){
@@ -52,26 +53,49 @@ int main(){
             break;
         }
         //division of two numbers
-        case '^': {
+        case 5: {
             printf("Enter a number: ");
             scanf("%lf", &operand1);
             printf("What power do you want to raise it to? ");
-            scanf("%d", &operand2);
-            power(operand1, operand2);
+            scanf("%d", &power_call);//get the power from user
+            power(operand1, power_call);  //call the power function
             break;
         }
-        case '!':{
+        case 6:{
             printf("Enter a number: ");
             scanf("%lf", &operand1);
             factorial(operand1);
             break;
         }
+        case 7:{
+            printf("Exiting the program.");
+            exit(0);
+            break;
+        }
         default:
-            printf("Error!!!Invalid operator.");
+            printf("Error!!!Invalid operator.\n");
+            exit(0);
             //default case
     }
-    printf("\nEnd of the program.");
+    printf("\nThanks for using the calculator.\n");
+    printf("Developed by: Samrat Parajuli");
     return 0;
+}
+
+//function to print main menu
+int PrintMenu(){
+    int choice;
+    printf("\t\t\tSimple Calculator\n");    //title
+    printf("What do you wish to do?\n");
+    printf("1.Addition\n");
+    printf("2.Subtraction\n");  
+    printf("3.Multiplication\n");
+    printf("4.Division\n"); 
+    printf("5.Power\n");
+    printf("6.Factorial\n");    
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+    return choice;
 }
 //functions for each operator
 void Sum(double a, double b){
