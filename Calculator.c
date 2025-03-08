78b5b639fe2bd8c5ff5bc48190273f229a3222e4
Samzt-13 <code.samzt13@gmail.com>
2025-03-08 19:52:00 +0545

    Added Features
    Added many new features

--- Calculator.c
+++ Calculator.c
@@ -9,13 +9,19 @@ void Diff(double, double);
 void Mult(double, double);
 void Div(double, double);
 void power(double, int);
-void factorial(double);
+void factorial(int);
+void SquareRoot(void);
+void Trigonometry(void);
+
+double to_radians(double);
+
+#define M_PI 3.14159265358979323846
 
 int main(){
     double operand1, operand2;
-    int power_call;
-    char operator = 1;
-
+    int power_call, facto_user; //define power_call for power & facto_user to get factorial from user as an integer
+    int operator = 1;
+    while(1){
     operator = PrintMenu(); //get the operator & print the menu
     //switch statement for operator
     switch(operator){
@@ -63,36 +69,47 @@ int main(){
         }
         case 6:{
             printf("Enter a number: ");
-            scanf("%lf", &operand1);
-            factorial(operand1);
+            scanf("%d", &facto_user);
+            factorial(facto_user);
             break;
         }
         case 7:{
-            printf("Exiting the program.");
-            exit(0);
+            SquareRoot();
             break;
         }
-        default:
-            printf("Error!!!Invalid operator.\n");
+        case 8:{
+            Trigonometry();
+            break;
+        }
+        case 9:{
+            printf("Exiting the program.");
+            printf("\nThanks for using the calculator.\n");
+            printf("Developed by: Samrat Parajuli");
+            printf("\n\t\t\t\t\tTHE END\n");
             exit(0);
-            //default case
+        }
+        default:
+            fprintf(stderr, "Error!!!Invalid operator.\n");
+            continue;            //default case
     }
-    printf("\nThanks for using the calculator.\n");
-    printf("Developed by: Samrat Parajuli");
+    }
     return 0;
 }
 
 //function to print main menu
 int PrintMenu(){
     int choice;
-    printf("\t\t\tSimple Calculator\n");    //title
+    printf("\n\t\t\t\t\tSimple Calculator\n");    //title
     printf("What do you wish to do?\n");
     printf("1.Addition\n");
     printf("2.Subtraction\n");  
     printf("3.Multiplication\n");
     printf("4.Division\n"); 
     printf("5.Power\n");
-    printf("6.Factorial\n");    
+    printf("6.Factorial\n");
+    printf("7.Square Root\n");
+    printf("8.Trigonometric Calculation\n");
+    printf("9.Exit\n");
     printf("Enter your choice: ");
     scanf("%d", &choice);
     return choice;
@@ -119,10 +136,71 @@ void power(double a, int b){
     double result = pow(a, b);
     printf("%.2lf ^ %d = %.2lf", a, b, result);
 }
-void factorial(double a){
+void factorial(int a){
     double fact = 1;
+    if (a < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.\n");
+        return;
+    }
     for(int i = 1; i <= a; i++){
         fact *= i;
     }
-    printf("%.2lf! = %.2lf", a, fact);
+    printf("%d! = %.2lf\n", a, fact);
+}
+
+void SquareRoot(){
+    int number;
+    double result;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    result = sqrt(number);
+    printf("Square Root of %d is: %.2lf", number, result);
+}
+
+void Trigonometry(){
+    int choice;
+    double angle, result;
+    printf("What type of Trigonometric Calculation do you want to do?\n");
+    printf("1.Sine\n");
+    printf("2.Cosine\n");
+    printf("3.Tangent\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    printf("Enter your angle: ");
+    scanf("%lf", &angle);
+    switch(choice){
+        case 1:{
+            result = sin(to_radians(angle));
+            printf("Sine(%.2lf) = %.2lf\n", angle, result);
+            break;
+        }
+
+        case 2:{
+            result = cos(to_radians(angle));
+            printf("Cosine(%.2lf) = %.2lf\n", angle, result);
+            break;
+        }
+
+        case 3:{
+            result = tan(to_radians(angle));
+            if(result < -1 || result > 1){
+                printf("tan(%.2lf) = Undefine\n", angle);
+            }
+            else{
+            printf("tan(%.2lf) = %.2lf\n", angle, result);
+            }
+            break;
+        }
+
+        default:{
+            printf("Invalid choice.\n");
+            break;
+        }
+    }
+}
+
+double to_radians(double num){
+    double fun_result = num * M_PI / 180;
+    return fun_result;
 }
